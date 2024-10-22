81c10ddf3b454434627895ac5c9e130d539be4b6
ahntuann <ahntuan284@gmail.com>
2024-10-22 16:55:18 +0700

    Build API: get chapter number by chapter id

--- src/main/java/dao/ChaptersDAO.java
+++ src/main/java/dao/ChaptersDAO.java
@@ -27,6 +27,39 @@ public class ChaptersDAO extends MyDAO {
         }
         return chapters;
     }
+    
+    private Chapters createChapter(ResultSet rs) {
+        Chapters chapter = new Chapters();
+
+        try {
+            while (rs.next()) {
+                int chapterId = rs.getInt("chapter_id");
+                int storyId = rs.getInt("story_id");
+                String chapterNumber = rs.getString("chapter_number");
+                chapter = new Chapters(chapterId,storyId,chapterNumber);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return chapter;
+    }
+    
+    public Chapters getChapterById(int chapterID) {
+        Chapters chapter = new Chapters();
+        String xSql = "select chapter_id,story_id,chapter_number from Chapters where chapter_id = ?";
+        
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, chapterID);
+            rs = ps.executeQuery();
+            chapter = createChapter(rs);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        return chapter;
+    }
+    
     //lấy danh sách các chapter của 1 truyện dựa vào storyId
 
     public List<Chapters> getChapterByStoryId(int storyId) {
